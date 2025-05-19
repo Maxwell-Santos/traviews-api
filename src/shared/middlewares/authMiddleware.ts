3be222ebcfe30df02adc4b3ed79f3388048f9412
Maxwell-Santos <max.coding011@gmail.com>
2025-05-18 22:51:06 -0300

    novo

--- src/shared/middlewares/authMiddleware.ts
+++ src/shared/middlewares/authMiddleware.ts
@@ -1,3 +1,33 @@
+// import { jwt } from 'jsonwebtoken'
 import passport from 'passport'
+import { User } from '../../modules/user/domain/entities/User'
 
 export const authenticateJWT = passport.authenticate('jwt', { session: true })
+
+// async function authMiddleware(req, res, next) {
+//   const accessToken = req.headers.authorization?.split(' ')[1]
+
+//   try {
+//     const payload = jwt.verify(accessToken, process.env.JWT_SECRET)
+//     req.user = await User.findById(payload.id)
+//     return next()
+//   } catch (err) {
+//     if (err.name === 'TokenExpiredError') {
+//       const refreshToken = req.cookies.refreshToken
+//       if (!refreshToken) return res.sendStatus(401)
+
+//       try {
+//         const payload = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET)
+//         const newAccessToken = generateAccessToken({ _id: payload.id })
+
+//         res.setHeader('x-access-token', newAccessToken) // ou setar no cookie
+//         req.user = await User.findById(payload.id)
+//         return next()
+//       } catch {
+//         return res.sendStatus(403) // refresh token inválido
+//       }
+//     }
+
+//     return res.sendStatus(401)
+//   }
+// }
